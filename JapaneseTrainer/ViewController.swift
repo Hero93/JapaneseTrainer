aa580d7d7da9bccf3dadfcc0eb9faa1aaa277868
luca.gramaglia <1993luca@gmail.com>
2016-04-28 22:48:56 +0200

    Initial Commit

---
+++ JapaneseTrainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JapaneseTrainer
+//
+//  Created by Luca Gramaglia on 28/04/16.
+//  Copyright © 2016 Luca Gramaglia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
